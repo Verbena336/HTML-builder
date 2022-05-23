c66d19575f8d0dce46e2d41fc80eb4ebf4b7c8f4
Natalia Byzova <nata22011997@gmail.com>
2022-05-23 15:07:23 +0300

    refactor: rework 6 task function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,70 +1,77 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
-async function copyHTML() {
-  await fs.promises.rm(path.resolve(__dirname, 'project-dist', 'index.html'), {recursive: true, force: true});
-  fs.mkdir(path.resolve(__dirname, 'project-dist'), {recursive: true}, (err) => {
-    if (err) return console.error(err);
-  });
-  const htmlReadStream = fs.createReadStream(path.resolve(__dirname, 'template.html'));
-  const htmlWriteStream = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'index.html'));
-  let str = await stringifyTemplate(htmlReadStream);
-  const htmlArray = str.split('\n');
-  for(let str of htmlArray) {
+async function copyHTML(input, projectDist) {
+  await fs.promises.rm(projectDist, {recursive: true, force: true});
+  await fs.promises.mkdir(projectDist, {recursive: true});
+
+  await copyFolder(path.resolve(__dirname, 'assets'), path.resolve(__dirname, 'project-dist', 'assets'));
+  await copyStyle(path.resolve(__dirname, 'styles'), path.resolve(__dirname, 'project-dist', 'style.css'));
+  const htmlReadStream = fs.createReadStream(input);
+  const htmlWriteStream = fs.createWriteStream(path.resolve(projectDist, 'index.html'));
+  // let str = await new Promise((resolve) => htmlReadStream.on('data', (data) => {
+  //   resolve(data.toString());
+  // }));
+  const stringify = readline.createInterface(htmlReadStream);
+  for await (let str of stringify) {
     if(str.trim().startsWith('{{') && str.trim().endsWith('}}')) {
-      console.log(str.trim().slice(2, -2));
       const fileName = str.trim().slice(2, -2);
-      await asyncPipe(fs.createReadStream(path.resolve(__dirname, 'components', `${fileName}.html`)), htmlWriteStream);
+      let readStream;
+      try {
+        await fs.promises.access(path.resolve(__dirname, 'components', `${fileName}.html`), fs.constants.R_OK);
+        readStream = fs.createReadStream(path.resolve(__dirname, 'components', `${fileName}.html`));
+      } catch(err) {
+        console.log(err.message);
+        continue;
+      }
+      readStream.pipe(htmlWriteStream, {end: false});
+      await new Promise(resolve => readStream.on('close', resolve));
       htmlWriteStream.write('\n');
     } else {
-      htmlWriteStream.write(str);
+      htmlWriteStream.write(str + '\n');
     }
   }
 }
 
-const stringifyTemplate = async (htmlStream) => {
-  return new Promise((resolve) => htmlStream.on('data', (data) => {
-    resolve(data.toString());
-  }));
-};
-
-async function copyStyle() {
-  await fs.promises.rm(path.resolve(__dirname, 'project-dist', 'style.css'), {recursive: true, force: true});
-  const outputPath = path.resolve(__dirname, 'project-dist', 'style.css');
-  const output = fs.createWriteStream(outputPath);
-  const inputPath = path.resolve(__dirname, 'styles');
-  const data = await fs.promises.readdir(inputPath, {withFileTypes: true});
-  for(let file of data) {
-    const filePath = path.resolve(inputPath, file.name);
+async function copyStyle(input, bundle) {
+  const output = fs.createWriteStream(bundle);
+  let data;
+  try {
+    data = await fs.promises.readdir(input, {withFileTypes: true});
+  } catch (err) {
+    console.log(err.message);
+    return;
+  }
+  for await (let file of data) {
+    const filePath = path.resolve(input, file.name);
     if(file.isFile() && path.extname(filePath) === '.css') {
-      await asyncPipe(fs.createReadStream(path.resolve(inputPath, file.name)), output);
+      const readStream = fs.createReadStream(path.resolve(input, file.name));
+      readStream.pipe(output, {end: false});
+      await new Promise(resolve => readStream.on('close', resolve));
       output.write('\n');
     }
   }
 }
 
-const asyncPipe = (rs, ws) => {
-  rs.pipe(ws, {end: false});
-  return new Promise(resolve => rs.on('end', resolve));
-};
-
 async function copyFolder(fromFolder, toFolder) {
-  await fs.promises.rm(toFolder, {recursive: true, force: true});
-  fs.mkdir(toFolder, {recursive: true}, (err) => {
-    if (err) return console.error(err);
+  await fs.promises.mkdir(toFolder, {recursive: true}, (err) => {
+    if (err) return console.log(err.message);
   });
   fs.readdir(fromFolder, {withFileTypes: true}, (err, files) => {
     if (err)
-      console.log(err);
+      console.log(err.message);
     else {
       files.forEach(file => {
         if(file.isFile()) {
-          fs.createReadStream(path.resolve(fromFolder, file.name)).pipe(fs.createWriteStream(path.resolve(toFolder, file.name)));
+          fs.copyFile(path.resolve(fromFolder, file.name), path.resolve(toFolder, file.name), (err) => {
+            if(err) console.log(err.message);
+          });
         } else if(file.isDirectory()) {
           try {
-            copyFolder(path.join(fromFolder, file.name), path.join(toFolder, file.name));
+            copyFolder(path.resolve(fromFolder, file.name), path.resolve(toFolder, file.name));
           } catch(err) {
-            console.error(err);
+            console.log(err.message);
           }
         }
       });
@@ -73,9 +80,7 @@ async function copyFolder(fromFolder, toFolder) {
 }
  
 try {
-  copyHTML();
-  copyFolder(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
-  copyStyle();
+  copyHTML(path.resolve(__dirname, 'template.html'), path.resolve(__dirname, 'project-dist'));
 } catch(err) {
-  console.error(err);
+  console.error(err.message);
 }
